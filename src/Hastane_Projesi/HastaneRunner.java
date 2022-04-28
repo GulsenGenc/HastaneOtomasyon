09bf614dd623197b6fb65ee72ff623283d2daa11
Meryem Akay <haceryol491@gmail.com>
2022-04-28 12:08:19 +0300

    meryem

--- src/Hastane_Projesi/HastaneRunner.java
+++ src/Hastane_Projesi/HastaneRunner.java
@@ -3,6 +3,7 @@ package Hastane_Projesi;
 import java.util.Arrays;
 
 public class HastaneRunner {
+private static Hastane obj=new Hastane();
     public static void main(String[] args) {
 
         String unvan = doktorUnvan("Baş ağrısı");
@@ -16,6 +17,21 @@ public class HastaneRunner {
     }
 
     public static String doktorUnvan(String actuelDurum) {
+
+        if(actuelDurum.equals("Allerji")){
+            return obj.unvanlar[0];
+        } else if(actuelDurum.equals("Bas agrisi")){
+            return obj.unvanlar[1];
+        }else if(actuelDurum.equals("Diabet")){
+            return obj.unvanlar[2];
+        }else if(actuelDurum.equals("Soguk alginiligi")){
+            return obj.unvanlar[3];
+        }else if(actuelDurum.equals("Migren")){
+            return obj.unvanlar[4];
+        }else if(actuelDurum.equals("Kalp hastaliklari")){
+            return obj.unvanlar[5];
+        }
+
         return "yanlış unvan";
     }
 
